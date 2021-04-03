df32058016ac299faf650442ab211bcde8776db3
Xingxin HE <xingxin.he@mail.polimi.it>
2021-04-04 00:18:35 +0800

    210404 Intro

---
+++ Introduction/Introduction/I1_randomWalker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Introduction
+{
+    public class randomWalker : GH_Component
+    {
+
+        public Walker walker = new Walker(0,0);
+        public List<Point3d> randomWalkers = new List<Point3d>();
+
+        public randomWalker()
+          : base("Random Walker", "RndWalker",
+              "The random walker in the intro section",
+              "NOC", "Intro")
+        {
+        }
+
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Reset", "Reset", "Reset the walker", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddPointParameter("Walker", "Walker", "The random walker as a point", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //retrieve input from gh
+            bool iReset = false;
+
+            DA.GetData("Reset", ref iReset);
+
+            walker.Step();
+
+            if (iReset)
+            {
+                walker.Reset();
+            }
+
+            DA.SetData("Walker", new Point3d(walker.x, walker.y, 0));
+
+
+
+            
+        }
+
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("1bd923c2-ff50-4a6a-9021-67800732e591"); }
+        }
+    }
+}
---
+++ Introduction/Introduction/Walker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    public class Walker
+    {
+        //Declare fields
+        public double x;
+        public double y;
+        //public bool Move = false;
+        private Random dice = new Random();
+
+        //constructor
+
+        public Walker(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        //method
+        public void Step()
+        {
+                int choice = (int)dice.Next(0, 4);
+                switch (choice)
+                {
+                    case 0:
+                        x++;
+                        break;
+                    case 1:
+                        x--;
+                        break;
+                    case 2:
+                        y++;
+                        break;
+                    case 3:
+                        y--;
+                        break;
+                }
+            
+        }
+        public void Reset()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+    }
+}
