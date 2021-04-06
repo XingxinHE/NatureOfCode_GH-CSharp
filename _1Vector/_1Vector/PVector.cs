923d13b0a03d854b8e552b80fa4536545ac79fbf
Xingxin HE <xingxin.he@mail.polimi.it>
2021-04-06 12:44:18 +0800

    chapter 01 vector

---
+++ _1Vector/_1Vector/PVector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Vector
+{
+    public class PVector
+    {
+        public double x;
+        public double y;
+
+        public PVector(double x_, double y_)
+        {
+            this.x = x_;
+            this.y = y_;
+        }
+
+        public void Add(PVector v)
+        {
+            x += v.x;
+            y += v.y;
+        }
+
+        public void Sub(PVector v)
+        {
+            x -= v.x;
+            y -= v.y;
+        }
+    }
+}
