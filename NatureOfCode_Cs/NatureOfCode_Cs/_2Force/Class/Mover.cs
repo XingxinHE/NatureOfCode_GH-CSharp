1fc8a1f9de572e7a36d91ab72d662b26d4fb7164
Xingxin HE <xingxin.he@mail.polimi.it>
2021-04-08 23:24:17 +0800

    mover

---
+++ NatureOfCode_Cs/NatureOfCode_Cs/_2Force/Class/Mover.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NatureOfCode_Cs._1Vector;
+
+namespace NatureOfCode_Cs._2Force
+{
+    public class Mover
+    {
+        public PVector location;
+        public PVector velocity;
+        public PVector acceleration;
+        public double mass;
+
+        public Mover(double m, double x, double y)
+        {
+            this.mass = m;
+            this.location = new PVector(x, y);
+            this.velocity = new PVector(0, 0);
+            this.acceleration = new PVector(0, 0);
+        }
+
+        public void ApplyForce(PVector force)
+        {
+            PVector f = new PVector(force.x / mass, force.y / mass);
+            acceleration.Add(f);
+        }
+
+        public void Update()
+        {
+            velocity.Add(acceleration);
+            location.Add(velocity);
+            acceleration.Mul(0);
+        }
+
+        public void CheckEdges(double width, double height)
+        {
+            if (location.x > width)
+            {
+                location.x = width;
+                velocity.x *= -1;
+            }
+            else if (location.x < 0)
+            {
+                velocity.x *= -1;
+                location.x = 0;
+            }
+
+            if (location.y > height)
+            {
+                location.y = height;
+                velocity.y *= -1;
+            }
+            else if (location.y < 0)
+            {
+                velocity.y *= -1;
+                location.y = 0;
+            }
+
+        }
+    }
+}
---
+++ NatureOfCode_Cs/NatureOfCode_Cs/_2Force/Component/MangMover.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace NatureOfCode_Cs._2Force.Component
+{
+    public class MangMover : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MangMover class.
+        /// </summary>
+        public MangMover()
+          : base("MangMover", "Nickname",
+              "Description",
+              "Category", "Subcategory")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var rand = new Random();
+
+            List<Mover> movers = new List<Mover>();
+            for (int i = 0; i < 10; i++)
+            {
+                Mover m = new Mover(rand.NextDouble() * (0.1 - 5) + 0.1, 0, 0);
+                movers.Add(m);
+
+
+            }
+
+            //movers.Count
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("5d058199-0dad-415b-a6e9-4b7864b42ca6"); }
+        }
+    }
+}
